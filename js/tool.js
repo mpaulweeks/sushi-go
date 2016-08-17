656020f75962e83cc79385468ec8d2639f1c581f
mpaulweeks <mpaulweeks@gmail.com>
2016-08-17 18:52:40 -0400

    add url reading for easy sim toggle

--- js/tool.js
+++ js/tool.js
@@ -1,4 +1,27 @@
 
+function readUrlParam(param_name, as_list){
+    as_list = as_list || false;
+    var vars = {};
+    var q = document.URL.split('?')[1];
+    if(q != undefined){
+        q = q.split('&');
+        for(var i = 0; i < q.length; i++){
+            var param = q[i].split('=');
+            var name = param[0];
+            var value = param[1];
+            vars[name] = vars[name] || [];
+            vars[name].push(value);
+        }
+    }
+    if (vars.hasOwnProperty(param_name)){
+        if (vars[param_name].length == 1 && !as_list){
+            return vars[param_name][0];
+        }
+        return vars[param_name];
+    }
+    return null;
+}
+
 function println(message){
     console.log(message);
     $('#out').append(message + '<br/>');
