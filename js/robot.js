6f3b2e5d0366f20ac45a87b5a1825f7dee7bee49
mpaulweeks <mpaulweeks@gmail.com>
2016-08-22 21:50:41 -0400

    add AdamBot

--- js/robot.js
+++ js/robot.js
@@ -13,6 +13,75 @@ function genRobot(name, prefFunc){
     return self;
 }
 
+module.AdamBot = genRobot('AdamBot', function(tally, pack, hands){
+    var remainingPicks = pack.length - 1;
+    var pref = [];
+
+    // early picks
+    if (tally[SASHIMI.id] % 3 == 2){
+        pref.push(SASHIMI);
+    }
+    if (tally.pendingWasabi > 0){
+        pref.push(NIGIRI_SQUID);
+    }
+    if (tally[DUMPLING.id] % 5 == 4){
+        pref.push(DUMPLING);
+    }
+    if (tally[SASHIMI.id] % 3 == 1 && remainingPicks >= 5){
+        pref.push(SASHIMI);
+    }
+    if (tally.pendingWasabi > 0){
+        pref.push(NIGIRI_SALMON);
+    }
+    if (tally[TEMPURA.id] % 2 == 1){
+        pref.push(TEMPURA);
+    }
+    if (tally[DUMPLING.id] % 5 == 3){
+        pref.push(DUMPLING);
+    }
+    if (tally[DUMPLING.id] % 5 == 2){
+        pref.push(DUMPLING);
+    }
+    pref.push(NIGIRI_SQUID);
+    if (remainingPicks >= 5 && tally.pendingWasabi == 0){
+        pref.push(WASABI);
+    }
+    if (tally[SASHIMI.id] % 3 == 0 && remainingPicks >= 7){
+        pref.push(SASHIMI);
+    }
+    if (tally[TEMPURA.id] % 2 == 0 && remainingPicks >= 5){
+        pref.push(TEMPURA);
+    }
+    if (tally[DUMPLING.id] % 5 == 3){
+        pref.push(DUMPLING);
+    }
+    if (tally[DUMPLING.id] % 5 == 2){
+        pref.push(DUMPLING);
+    }
+    if (tally.pendingWasabi > 0){
+        pref.push(NIGIRI_EGG);
+    }
+    if (remainingPicks >= 2 + tally[CHOPSTICKS.id]){
+        pref.push(CHOPSTICKS);
+    }
+
+    // else
+    pref.push(NIGIRI_SALMON);
+    pref.push(DUMPLING);
+    pref.push(MAKI_3);
+    pref.push(NIGIRI_EGG);
+    pref.push(MAKI_2);
+    pref.push(CHOPSTICKS);
+    pref.push(PUDDING);
+    pref.push(MAKI_1);
+
+    // dumping ground
+    pref.push(TEMPURA);
+    pref.push(SASHIMI);
+    pref.push(WASABI);
+    return pref;
+});
+
 module.PaulBot = genRobot('PaulBot', function(tally, pack, hands){
     var remainingPicks = pack.length - 1;
     var pref = [];
